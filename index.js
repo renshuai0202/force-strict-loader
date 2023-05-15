81c964a50f3fb448978d6d0de18c9cdf2c83ea89
帅帅 <renshuai0202@gmail.com>
2023-05-15 09:30:49 +0800

    feat: 假装使用source-map

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var loaderUtils = require("loader-utils");
 
-module.exports = function(content) {
+module.exports = function(content, sourceMap) {
   // 2. 开启缓存
   if (this.cacheable) {
     this.cacheable();
@@ -8,6 +8,12 @@ module.exports = function(content) {
   // 3. 获取options
   const options = loaderUtils.getOptions(this) || {};
   console.log('options', options);
+  // 应用source-map
+  if (options.sourceMap && sourceMap) {
+    // ...
+    // 假装实现了
+    console.log('force-strict-loader假装已使用source-map');
+  }
   // 1. 为每个JS开起严格模式
   const useStrictPrefix = '\'use strict\';\n\n';
   return useStrictPrefix + content;
