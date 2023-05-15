7c1cdc593d44963cca313a7027b89e002d23af4b
帅帅 <renshuai0202@gmail.com>
2023-05-15 09:14:09 +0800

    feat: 安装并使用loader-utils

--- README.md
+++ README.md
@@ -22,3 +22,12 @@ module.exports = function(content) {
 
 当输入的文件和其依赖，没有发生变化时，使用缓存，不再重新转换
 提升打包速度
+
+3. 启用source-map
+作用：在浏览器开发者工具中，查看源码。
+优点：便于排查问题
+缺点：降低构建速度，增加输出文件体积
+用途：用于开发环境
+
+安装@1.4.0并使用 `loader-utils` 
+loader-utils 是webpack工具库，用于开发和调试loader
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+var loaderUtils = require("loader-utils");
+
 module.exports = function(content) {
   // 2. 开启缓存
   if (this.cacheable) {
     this.cacheable();
   }
+  // 3. 获取options
+  const options = loaderUtils.getOptions(this) || {};
+  console.log('options', options);
   // 1. 为每个JS开起严格模式
   const useStrictPrefix = '\'use strict\';\n\n';
   return useStrictPrefix + content;
